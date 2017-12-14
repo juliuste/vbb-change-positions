961b894b51355e2c18b46e1eee32aebf275ae3c7
Jannis R <mail@jannisr.de>
2017-12-14 19:50:08 +0100

    validate station IDs

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ const parseCsv = require('csv-parser')
 const toArray = require('get-stream').array
 const path = require('path')
 const fs = require('fs')
+const getStations = require('vbb-stations')
 
 const changePositions = require('.')
 
@@ -57,16 +58,22 @@ test('data.csv contains correct values', async (t) => {
 		else if (p.station) desc += ' (' + p.station + ')'
 
 		t.ok(notNullString(p.station), desc + ' station')
+		const [s] = getStations(p.station)
+		t.ok(s, desc + ' station')
 		t.ok(nullString(p.stationName), desc + ' stationName')
 
 		t.ok(notNullString(p.fromLine), desc + ' fromLine')
 		t.ok(notNullString(p.fromStation), desc + ' fromStation')
+		const [fS] = getStations(p.fromStation)
+		t.ok(fS, desc + ' station')
 		t.ok(nullString(p.fromStationName), desc + ' fromStationName')
 		t.ok(nullString(p.fromTrack), desc + ' fromTrack')
 		t.ok(validPosition(p.fromPosition), desc + ' fromPosition')
 
 		t.ok(notNullString(p.toLine), desc + ' toLine')
 		t.ok(notNullString(p.toStation), desc + ' toStation')
+		const [tS] = getStations(p.toStation)
+		t.ok(tS, desc + ' station')
 		t.ok(nullString(p.toStationName), desc + ' toStationName')
 		t.ok(nullString(p.toTrack), desc + ' toTrack')
 		t.ok(validPosition(p.toPosition), desc + ' toPosition')
