f7fe0574affad3aadfa3460c3029cfe66ecb0c82
Jannis R <mail@jannisr.de>
2018-01-27 18:55:48 +0100

    fix relative data.ndjson path :bug:

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 const ndjson = require('ndjson')
 const toArray = require('get-stream').array
 
 const positions = () => toArray(
-    fs.createReadStream('./data.ndjson')
+    fs.createReadStream(path.join(__dirname, 'data.ndjson'))
     .pipe(ndjson.parse())
 )
 
