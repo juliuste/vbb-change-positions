9b6ec3a7893cd35a799a354011ae1798bfee3e10
Jannis R <mail@jannisr.de>
2018-01-28 13:23:25 +0100

    promise -> readable stream :boom:

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs')
 const ndjson = require('ndjson')
-const toArray = require('get-stream').array
 
-const positions = () => toArray(
-    fs.createReadStream('./data.ndjson')
+const positions = () => {
+    return fs.createReadStream('./data.ndjson')
     .pipe(ndjson.parse())
-)
+}
 
 module.exports = positions
