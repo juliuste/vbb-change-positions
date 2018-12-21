b3ab4515b0ea3a0e2caae8780c9969e2fae987b5
Jannis R <mail@jannisr.de>
2018-12-21 13:23:39 +0100

    build JSON file, browser compatibility

---
+++ build.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const {join} = require('path')
+const {createReadStream} = require('fs')
+const {parse: parseNdjson} = require('ndjson')
+const {array: asArray} = require('get-stream')
+
+const onError = (err) => {
+	if (err) {
+		console.error(err)
+		process.exit(1)
+	}
+}
+
+const src = join(__dirname, 'data.ndjson')
+const dest = join(__dirname, 'data.json')
+
+const readable = createReadStream(src)
+.once('error', onError)
+.pipe(parseNdjson())
+.once('error', onError)
+
+asArray(readable)
+.then((data) => {
+	process.stdout.write(JSON.stringify(data) + '\n')
+})
+.catch(onError)
