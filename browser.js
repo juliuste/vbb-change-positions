36e99dd5342213c649b6b645de72d497c668d1d0
Jannis R <mail@jannisr.de>
2022-11-30 18:22:19 +0100

    from2-array -> stream.Readable

--- browser.js
+++ browser.js
@@ -1,8 +1,24 @@
 'use strict'
 
-const fromArray = require('from2-array')
+const {Readable} = require('stream')
 const data = require('./data.json')
 
-const positions = () => fromArray.obj(data)
+const positions = () => {
+	const lastI = data.length - 1
+
+	let i = 0
+	function read (amount) {
+		const nextI = Math.min(i + amount, lastI)
+		for (; i <= nextI; i++) {
+			this.push(data[i])
+		}
+		if (i >= lastI) this.push(null) // end
+	}
+
+	return new Readable({
+		objectMode: true,
+		read,
+	})
+}
 
 module.exports = positions
